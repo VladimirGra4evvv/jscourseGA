ff76be2b6e7479fe62410361a695b374b9219edb
Vladimir Gra4ev <gra4evvv@yandex.ru>
2023-08-21 23:06:05 +0300

    выполнено ДЗ урок 3

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-const title = prompt('Как называется ваш проект?', "Проект 1");
-const screens = prompt('Какие типы экранов нужно разработать?', "Простые, Сложные, Интерактивные");
-const screenPrice = prompt('Сколько будет стоить данная работа?', "12000");
+'use strict'
+
+const title = prompt('Как называется ваш проект?', 'Разработка сайта');
+const screens = prompt('Какие типы экранов нужно разработать?', 'Сложные');
+const screenPrice = prompt('Сколько будет стоить данная работа?', '30000');
 const adaptive = prompt('Нужен ли адаптив на сайте?');
-const rollback = 20;
-const fullPrice = 85000;
+const rollback = 20; //откат за посредника
+let fullPrice; //полная стоимость
+let servicePercentPrice; //полная стоимость за вычетом отката
+
+const qustions1 = prompt('Какой дополнительный тип услуги нужен?', 'Установка на хост');
+let servicePrice1 = prompt('Сколько это будет стоить?', '3000');
+const qustions2 = prompt('Какой дополнительный тип услуги нужен?', 'Настройка HTTPS');
+let servicePrice2 = prompt('Сколько это будет стоить?', '5000');
 
 
 console.log(typeof title);
@@ -24,19 +32,37 @@ console.log(screens.toLowerCase());
 console.log('\n');
 console.log(screens.split(" "));
 console.log('\n');
-console.log("Процент отката посреднику за работу " + (fullPrice * (rollback / 100)) + " RUB");
 console.log('\n');
 console.log('\n');
-console.log(title);
-console.log(screens);
-console.log(screenPrice);
+
+
 if (adaptive == 'да') {
     console.log(Boolean(true));
 } else
     console.log(Boolean(false));
 
+console.log('\n');
 
+fullPrice = Number(screenPrice) + Number(servicePrice1) + Number(servicePrice2);
 
+fullPrice = Math.round(fullPrice)
+console.log("Процент отката посреднику за работу " + (fullPrice * (rollback / 100)) + " RUB");
+console.log("Cтоимость работы: " + fullPrice + " RUB");
 
+servicePercentPrice = fullPrice - fullPrice * (rollback / 100);
 
+console.log("Итоговая стоимость: " + servicePercentPrice + " RUB");
 
+switch (true) {
+    case servicePercentPrice > 30000:
+        console.log("Даем скидку в 10%");
+        break;
+    case 15000 < servicePercentPrice && servicePercentPrice < 30000:
+        console.log("Даем скидку в 5%");
+        break;
+    case 0 < servicePercentPrice && servicePercentPrice < 15000:
+        console.log("Скидка не предусмотрена");
+        break;
+    default:
+        console.log("Что то пошло не так");
+}
